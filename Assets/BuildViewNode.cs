7b34ab5c5d8e6afac199ab076b674cb9dd77c4a2
Berty Ruan <berty.ruan@gmail.com>
2015-09-15 17:56:52 -0400

    adds node image and node script

---
+++ Assets/BuildViewNode.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildViewNode : MonoBehaviour {
+
+    // Node Properties
+    private bool isStopSign = false;
+    private bool isRedLight = false;
+    private bool isSource = false;
+    private bool isSink = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // Select Node Property
+    void SetNodeProperty(string prop)
+    {
+
+    }
+
+    // Remove selected nodes
+    void RemoveSelected()
+    {
+
+    }
+
+    // Remove all nodes
+    void RemoveAll()
+    {
+
+    }
+}
