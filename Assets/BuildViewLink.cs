e8a8215559820887a83aa2d60cf80cf52eae6c8f
Haojun Sui <shjnyr@me.com>
2015-09-19 11:38:19 -0400

    Adds link script

---
+++ Assets/BuildViewLink.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildViewLink : MonoBehaviour {
+
+	// Link properties
+	private bool isLeftDirected = false;
+	private bool isRightDirected = false;
+	private bool isUndirected = true;
+	private BuildViewNode *left;
+	private BuildViewNode *right;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	// Select link property
+	void setLinkProperty(string prop) {
+
+	}
+
+	// Remove selected links
+	void removeSelected() {
+
+	}
+
+	// Remove all links
+	void removeAll() {
+
+	}
+}
