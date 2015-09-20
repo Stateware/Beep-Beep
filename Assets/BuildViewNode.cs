d106331ade678fc661e858397cb6278c0802d7c6
Berty Ruan <berty.ruan@gmail.com>
2015-09-19 21:05:35 -0400

    updates node GUI and makes buttons interactable

--- Assets/BuildViewNode.cs
+++ Assets/BuildViewNode.cs
@@ -1,50 +1,74 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+
 [RequireComponent(typeof(Rigidbody))]
 public class BuildViewNode : MonoBehaviour
 {
 
-    public Transform node;
+    public string[] nodeStrings = new string[] { "StopSign", "Traffic Light", "Source", "Sink" };
+    private enum NodeType { StopSign, TrafficLight, Source, Sink}
+    private NodeType nodeType;
+
+    public GameObject node;
+    private SphereCollider myCollider;
     private Rigidbody myRigidBody;
-    private enum NodeType  {
-        StopSign,
-        RedLight,
-        Source,
-        Sink
-    }
+    private Rect windowSize = new Rect(0, 0, Screen.width/4, Screen.height/4);
+    
     private bool isClicked;
 
+    //Pesudo Constructor for Node
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody>();
+        myCollider = gameObject.AddComponent<SphereCollider>();
+        myCollider.radius = 0.75f;
+        myRigidBody.useGravity = false;
     }
-
-    void Update()
-    {
-
-    }
-
 
     private void OnGUI()
     {
-        string msgtxt = "hello world!";
+        //Creates the Node Property Window
         if (isClicked)
-        {
-            GUI.Box(new Rect(200, 100, 320, 110), msgtxt);
-        }
+            windowSize = GUI.Window(0, windowSize, NodePropertyWindow, "Set Node Property");
     }
 
+    //Creates the buttons
+    private void NodePropertyWindow(int windowID)
+    {
+        GUILayout.BeginVertical();
+        GUILayout.Space(10);
+
+        if(GUILayout.Button(nodeStrings[0]))
+        {
+            nodeType = NodeType.StopSign;
+            Debug.Log(nodeType);
+        }
+        if (GUILayout.Button(nodeStrings[1]))
+        {
+            nodeType = NodeType.TrafficLight;
+            Debug.Log(nodeType);
+        }
+        if (GUILayout.Button(nodeStrings[2]))
+        {
+            nodeType = NodeType.Source;
+            Debug.Log(nodeType);
+        }
+        if (GUILayout.Button(nodeStrings[3]))
+        {
+            nodeType = NodeType.Sink;
+            Debug.Log(nodeType);
+        }
+
+        GUILayout.EndVertical();
+
+        GUI.DragWindow();
+    }
     
     void OnMouseUp()
     {
         // When you click, change the variables value
-        if (isClicked)
-        {
-            Debug.Log("Hello World");
-            isClicked = false;
-        }
-        else
-            isClicked = true;
+        if (isClicked) isClicked = false;
+        else isClicked = true;
     }
 }
