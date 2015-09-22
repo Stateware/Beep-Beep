81375e10bfcde923de479980aac4802f44080234
Haojun Sui <shjnyr@me.com>
2015-09-22 16:58:15 -0400

    Updates link feature which sticks link to both endpoints

---
+++ Assets/EditorView/BuildViewLink.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildViewLink : MonoBehaviour {
+
+	// Link properties
+	private bool isLeftDirected = false;
+	private bool isRightDirected = false;
+	private bool isUndirected = true;
+	public BuildViewNode origin;
+	public BuildViewNode destination;
+	private int laneNum = 1;
+
+	private LineRenderer lineRenderer;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		lineRenderer = GetComponent<LineRenderer>();
+		lineRenderer.SetPosition (0, origin.transform.position);
+		lineRenderer.SetWidth (.45f, .45f);
+
+		lineRenderer.SetPosition(1, destination.transform.position);
+	}
+
+	// Select link property
+	void setLinkProperty(string prop) {
+
+	}
+
+	// Remove selected links
+	void removeSelected() {
+
+	}
+
+	// Remove all links
+	void removeAll() {
+
+	}
+}
