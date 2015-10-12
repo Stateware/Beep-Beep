58361b8505b8e7f097dbf1bee4b32dea9462c33f
Aditya Naik <naik@psu.edu>
2015-10-12 14:10:50 -0400

    Adds script skeleton for spawning of cars

---
+++ Assets/SimView/CarSpawn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using static BuildViewNode;
+public class CarSpawn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+       
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    private class NodePrefab
+    {
+
+    }
+}
