fc1dfbf23745b246832ddbf364d08d1b93902ed2
Aditya Naik <naik@psu.edu>
2015-10-13 20:13:47 -0400

    Adds a play button and compiler script skeleton

---
+++ Assets/BuildView/Compiler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+this compiler should carry out the following tasks:
+-> check if the graph is connected
+--> if not, change sprite of nodes that are disconnected
+-> start simulation view by spawning cars
+*/
+
+public class Compiler : MonoBehaviour {
+    public BuildViewNode[] nodes;
+
+    void compile()
+    {
+        nodes = UnityEngine.Object.FindObjectsOfType<BuildViewNode>();
+        
+    }
+}
--- Assets/SimView/CarSpawn.cs
+++ Assets/SimView/CarSpawn.cs
@@ -12,7 +12,7 @@ public class CarSpawn : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-	
+	    
 	}
 
 }
