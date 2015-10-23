e7283616b0b79e737aeaf5037efd459fd3d26377
Aditya Naik <naik@psu.edu>
2015-10-23 14:39:55 -0400

    Reverts compiler.cs to functioning version

--- Assets/BuildView/Compiler.cs
+++ Assets/BuildView/Compiler.cs
@@ -1,48 +1,59 @@
-﻿using UnityEngine;
-using System.Collections;
-
-/*
-this compiler should carry out the following tasks:
--> check if the graph is connected
---> if not, change sprite of nodes that are disconnected
--> start simulation view by spawning cars
-*/
-
+﻿
 public class Compiler : MonoBehaviour {
-    public BuildViewNode[] nodes;
-
-    void compile()
-    {
-        nodes = UnityEngine.Object.FindObjectsOfType<BuildViewNode>();
-    }
-
-    public void compileToActionPoint()
-    {
-        this.GetComponent<SpriteRenderer>().sprite = this.getAssetForNode();
-        //new collision area must be redefined
-    }
-
-    private Sprite getAssetForNode()
-    {
-        string assetPathVar = this.generateAssetPath();
-        return (Sprite)Resources.Load(assetPathVar, typeof(Sprite));
-    }
-
-    private string generateAssetPath()
-    {
-        BuildViewNode allNodeProperties = GameObject.FindObjectOfType<BuildViewNode>();
-        if (allNodeProperties.getNodeProperty() == BuildViewNode.NodeType.TrafficLight)
-        {
-            return BuildViewNode.NodeType.TrafficLight.ToString();
-        }
-        else if (allNodeProperties.getNodeProperty() == BuildViewNode.NodeType.StopSign)
-        {
-            return BuildViewNode.NodeType.StopSign.ToString();
-        }
-        else if (allNodeProperties.getNodeGateTypeProperty() == BuildViewNode.NodeGateType.Source)
-        {
-            return BuildViewNode.NodeGateType.Source.ToString();
-        }
-        return BuildViewNode.NodeGateType.Sink.ToString();
-    }
+	public BuildViewNode[] nodes;
+	
+	public BuildViewLink[] links;
+	void compile()
+	{
+		nodes = UnityEngine.Object.FindObjectsOfType<BuildViewNode>();
+		
+		List< List<bool>> node_adj_matrix=new List< List<bool>>();
+		int index = 0;
+		foreach(object node in nodes)
+		{
+			node_adj_matrix.Add(new List<bool>());
+			foreach(object node_other in nodes)
+			{
+				node_adj_matrix[index].Add(false);
+			}
+			Console.WriteLine(node.ToString());
+			++index;
+		}
+		
+		links = UnityEngine.Object.FindObjectsOfType<BuildViewLink>();
+		foreach (object link in links)
+		{
+			
+		}
+	}
+	
+	public void compileToActionPoint()
+	{
+		this.GetComponent<SpriteRenderer>().sprite = this.getAssetForNode();
+		//new collision area must be redefined
+	}
+	
+	private Sprite getAssetForNode()
+	{
+		string assetPathVar = this.generateAssetPath();
+		return (Sprite)Resources.Load(assetPathVar, typeof(Sprite));
+	}
+	
+	private string generateAssetPath()
+	{
+		BuildViewNode allNodeProperties = GameObject.FindObjectOfType<BuildViewNode>();
+		if (allNodeProperties.getNodeProperty() == BuildViewNode.NodeType.TrafficLight)
+		{
+			return BuildViewNode.NodeType.TrafficLight.ToString();
+		}
+		else if (allNodeProperties.getNodeProperty() == BuildViewNode.NodeType.StopSign)
+		{
+			return BuildViewNode.NodeType.StopSign.ToString();
+		}
+		else if (allNodeProperties.getNodeGateTypeProperty() == BuildViewNode.NodeGateType.Source)
+		{
+			return BuildViewNode.NodeGateType.Source.ToString();
+		}
+		return BuildViewNode.NodeGateType.Sink.ToString();
+	}
 }
