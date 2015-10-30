796db8fce3f509aaca6bd63d58757d90fa8d59d1
Vincent Wang <v_wang@outlook.com>
2015-10-30 10:51:38 -0400

    1. Add background image; 2. Makes script for the car to change directions (Without rotation)

--- Assets/SimView/SimViewCar.cs
+++ Assets/SimView/SimViewCar.cs
@@ -10,12 +10,15 @@ public class SimViewCar : MonoBehaviour {
 	private float currRoadLength;
 
 	private Vector3 origin = new Vector3(-7, -3, 0);
-	private Vector3 destination = new Vector3(6, 6, 0);
+	private Vector3 destination = new Vector3(4, 4, 0);
+	
+	private Vector3[] path = {new Vector3(1, -3, 0), new Vector3(2, 2, 0), new Vector3(-3, -4, 0), new Vector3(2, 1, 0), new Vector3(-3, 5, 0)};
 
-
+	private int i = 0;
 	// Use this for initialization
 	void Start () {
 		transform.position = origin;
+		destination = path [i];
 	}
 	
 	// Update is called once per frame
@@ -23,7 +26,10 @@ public class SimViewCar : MonoBehaviour {
 		// Vector3 origin = roads [currRoadIndex].origin.transform.position;
 		// Vector3 destination = roads [currRoadIndex].destination.transform.position;
 		// transform.position = Vector3.Lerp(origin, destination, speed * Time.deltaTime);
-
+		if (transform.position == destination && i != 5) {
+			destination = path [i + 1];
+			i = i + 1;
+		}
 		transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
 	}
 }
