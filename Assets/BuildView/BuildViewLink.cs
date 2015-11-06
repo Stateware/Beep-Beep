e215e15db2c97960751e96e689c6a29613fa4799
Haojun Sui <shjnyr@me.com>
2015-11-06 10:51:42 -0500

    Adds comments in BuildViewLink.cs

--- Assets/BuildView/BuildViewLink.cs
+++ Assets/BuildView/BuildViewLink.cs
@@ -1,4 +1,14 @@
-﻿using UnityEngine;
+﻿////////////////////////////////////////////////////////////////////////////////
+//
+//	File			: BuildViewLink.cs
+//	Description		: This is the source code for BuildViewLink in BuildView
+//
+//	Author			: Haojun Sui, Yuehui Wang
+//	Last Modified	: Fri Nov  6 10:14:36 EST 2015
+//
+
+// Include Classes
+using UnityEngine;
 using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
@@ -9,25 +19,50 @@ public class BuildViewLink : MonoBehaviour {
 	private bool isLeftDirected = true;
 	private bool isRightDirected = false;
 	private bool isUndirected = false;
-	public Node origin;
-	public Node destination;
 	private int laneNum = 1;
+	private float angle;
 	private LineRenderer lineRenderer;
 	private BoxCollider lineCollider;
 	private float lineRendererWidth = 0.35f;
+
+	public Node origin;
+	public Node destination;
+	public Link link;
+
 	int originID;
 	int destinationID;
 	string toLogger;
-    public Link link;
+    
+	////////////////////////////////////////////////////////////////////////////////
+	//
+	// Function     	: Start
+	// Description  	: Initialize BuildViewLink
+	//
+	// Inputs       	: None
+	// Outputs      	: None
+	//
+	// Pre-conditions	: None
+	// Post-conditions	: Collider and LineRenderer set to BuildViewLink
 
-	// Initialization
 	void Start () {
 		lineRenderer = GetComponent<LineRenderer> ();
 		lineCollider = gameObject.AddComponent<BoxCollider> ();
         link = gameObject.AddComponent<Link>();
 	}
-	
-	// Update is called once per frame
+
+	////////////////////////////////////////////////////////////////////////////////
+	//
+	// Function     	: Update
+	// Description  	: Update BuildViewLink's collider's angle, and change its
+	//					  texture accordingly. Update is called once per frame
+	//
+	// Inputs       	: None
+	// Outputs      	: None
+	//
+	// Pre-conditions	: BuildViewLink successfully initialzed and has origin and
+	//					  destination set
+	// Post-conditions	: BuildViewLink's collider's angle and texture updated
+
 	void Update () {
 		// Store origin and destination position
 		Vector3 originPos = origin.transform.position;
@@ -35,6 +70,7 @@ public class BuildViewLink : MonoBehaviour {
 		
 		// Draw links
 		lineRenderer.SetWidth (lineRendererWidth, lineRendererWidth);
+
 		if (isUndirected) {
 			lineRenderer.SetPosition (0, originPos);
 			lineRenderer.SetPosition (1, destinationPos);
@@ -53,27 +89,26 @@ public class BuildViewLink : MonoBehaviour {
 		lineCollider.size = new Vector3 (lineLength, lineRendererWidth, 0);
 		lineCollider.transform.position = (originPos + destinationPos) / 2;
 
-		float angle = Mathf.Abs (originPos.y - destinationPos.y) / Mathf.Abs (originPos.x - destinationPos.x);
-
-		// If the angle is in second or fourth quadrant
-		if ((originPos.y - destinationPos.y) * (originPos.x - destinationPos.x) < 0)
-			angle *= -1;
-
-		angle = Mathf.Rad2Deg * Mathf.Atan (angle);
+		// Update angle of the collider for BuildViewLink
+		setAngle (originPos, destinationPos);
 
 		// Rotate lineCollider to lineRenderer's angle
 		lineCollider.transform.eulerAngles = new Vector3 (0, 0, angle);
 
+		// Tile the texture according to current length
 		lineRenderer.material.mainTextureScale = new Vector2(lineLength / 2, 1);
-		/*
-		if (!origin.IsConnected || !destination.IsConnected ) {
-			origin.IsConnected=true;
-			destination.IsConnected=true;
-		}
-*/
-		//toLogger = "Node #"+origin.name + " -> " + "Node #"+destination.name;
-		//Debug.Log (toLogger);
 	}
+
+	////////////////////////////////////////////////////////////////////////////////
+	//
+	// Function     	: OnMouseDrag
+	// Description  	: Move BuildViewLink, origin, and destination responsively
+	//
+	// Inputs       	: None
+	// Outputs      	: None
+	//
+	// Pre-conditions	: origin and destination has been assigned
+	// Post-conditions	: BuildViewLink, origin, and destination's positions were changed
 
 	void OnMouseDrag () {
 		Vector2 mousePosition = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
@@ -85,18 +120,25 @@ public class BuildViewLink : MonoBehaviour {
 		destination.transform.position += offPosition;
 	}
 
-	// Select link property
-	void setLinkProperty(string prop) {
+	////////////////////////////////////////////////////////////////////////////////
+	//
+	// Function     	: setAngle
+	// Description  	: Set collider's angle to current one
+	//
+	// Inputs       	: originPos - origin's Vector3 position
+	//					  destinationPos - destination's Vector3 position
+	// Outputs      	: None
+	//
+	// Pre-conditions	: angle is created
+	// Post-conditions	: BuildViewLink's collider's angle updated
 
-	}
-
-	// Remove selected links
-	void removeSelected() {
-
-	}
-
-	// Remove all links
-	void removeAll() {
-
+	void setAngle(Vector3 originPos, Vector3 destinationPos) {
+		angle = Mathf.Abs (originPos.y - destinationPos.y) / Mathf.Abs (originPos.x - destinationPos.x);
+		
+		// If the angle is in second or fourth quadrant
+		if ((originPos.y - destinationPos.y) * (originPos.x - destinationPos.x) < 0)
+			angle *= -1;
+		
+		angle = Mathf.Rad2Deg * Mathf.Atan (angle);
 	}
 }
