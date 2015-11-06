75acc891e53c4f68eb4e0a9d68001b0fc2b7e268
Berty Ruan <berty.ruan@gmail.com>
2015-11-06 11:03:42 -0500

    adds standard comments and header

--- Assets/BuildView/BuildViewNode.cs
+++ Assets/BuildView/BuildViewNode.cs
@@ -1,25 +1,41 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+// File Name: BuildViewNode.cs
+// Description: This Unity class defines all user interactions for node prefabs. 
+//              It also creates node prefabs when SpawnNewNode is called.
+// Dependencies: Gameobject: NodePrefab (Clone), Components: Node.c
+// Additional Notes: 
+
 public class BuildViewNode : MonoBehaviour
 {
     public Node node;
     public GameObject NodePrefab;
     private BuildViewSelectionHandler selectionHandler;
 
+    //Description: Sets up references between scripts 
+    //Pre: Script Components Node.c and BuildViewSelectionHandler.c exist
+    //Post: Gameobject NodePrefab (Clone) has a component Node.c. selectionHandler is initalized to the singleton script BuildViewSelectionHandler.c
     void Awake()
     {
         node = gameObject.AddComponent<Node>();
         selectionHandler = GameObject.FindObjectOfType<BuildViewSelectionHandler>();
     }
 
+    //Description: Changes the gameobject's position in the game world
+    //Pre:  Mouse cursor is over a NodePrefab (Clone) gameobject, and the left mouse button is held down for a duration of time. 
+    //Post: The gameobject's position is at the location where the left mouse button is released.
     void OnMouseDrag()
     {
         Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
         Vector2 objPosition = Camera.main.ScreenToWorldPoint(mousePosition);
-        transform.position = objPosition;
+        gameObject.transform.position = objPosition;
     }
-        
+
+    //Description: Modifies BuildViewSelectionHandler.c's selectedNodes List
+    //Pre: Mouse cursor is over a NodePrefab (Clone) gameobject, the left mouse button is held down for a duration of time, 
+    //      and finally the mouse button is released independent of the mouse's position.
+    //Post: Adds a unique gameobject to the selectedNodes list. If the gameobject is not unique, remove it from the list.
     void OnMouseUp()
     {
         // When you click, add self to Selection array. Works with creating links.
@@ -29,7 +45,9 @@ public class BuildViewNode : MonoBehaviour
             selectionHandler.RemoveNode(this);
      }
 
-    //define miscellaneous mouse functions
+    //Description: Destroys the NodePrefab (Clone) and any Link gameobjects connected to it
+    //Pre: Mouse cursor is over a NodePrefab (Clone) gameobject, and the right mouse button is clicked.
+    //Post: The NodePrefab (Clone) gameobject is destroyed and removed from the selectedNodes list.
     void OnMouseOver()
     {
         if (Input.GetMouseButton(1)) // for right mouse click
@@ -39,6 +57,9 @@ public class BuildViewNode : MonoBehaviour
         }
     }
 
+    //Description: Creates a NodePrefab (Clone) at the mouse's location
+    //Pre: The Node button in the gameworld is clicked 
+    //Post: A NodePrefab (Clone) is created and active in the gameworld.
     public void SpawnNewNode()
     {
         Vector2 mousePosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
