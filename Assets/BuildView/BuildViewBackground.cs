7eacae4a2faae49a9e73298c626a65398783358a
Andrew Lopreiato <drew.lopreiato@gmail.com>
2015-11-06 10:52:03 -0500

    Add comments and standard headers to BuildViewBackground

--- Assets/BuildView/BuildViewBackground.cs
+++ Assets/BuildView/BuildViewBackground.cs
@@ -1,9 +1,20 @@
-﻿using UnityEngine;
+﻿/* File Name:       BuildViewBackground.cs
+ * Description:     This script controls the interaction between the user and the background. This ensures that the
+ *                  background persists with the camera zoom changes, and movement, and calls the appropriate function
+ *                  when clicked.
+ * Dependencies:    The camera must have a BuildViewSelectionHandler component.
+ * Additional Notes:
+ */
+using UnityEngine;
 using System.Collections;
 using UnityEngine.EventSystems;
 
 public class BuildViewBackground : MonoBehaviour {
 
+    /* Description: Unity3D API function that is called when a mouse is released over this object.
+     * PRE:         The main camera has a BuildViewSelectionHandler component attached.
+     * POST:        The selection of nodes is cleared.
+     */
     void OnMouseUp()
     {
         if (!EventSystem.current.IsPointerOverGameObject())
@@ -12,12 +23,20 @@ public class BuildViewBackground : MonoBehaviour {
         }
     }
 
+    /* Description: Unity3D API function that is called when the frame is updated.
+     * PRE:         None.
+     * POST:        This object is scaled and positioned appropriately to the camera.
+     */
     void Update()
     {
         Resize();
     }
 
-    void Resize(bool keepAspect = false)
+    /* Description: Rescales the image attached to this object to fit the screen.
+    * PRE:          This object has a SpriteRenderer component with a defined sprite.
+    * POST:         The image fills the screen, or fills it as much to keep its aspect ratio.
+    */
+    private void Resize(bool keepAspect = false)
     {
         SpriteRenderer sr = GetComponent<SpriteRenderer>();
 
