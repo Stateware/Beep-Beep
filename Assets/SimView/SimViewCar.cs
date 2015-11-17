d6653b19d1ee8fa297e2b959dce3a9557d7dc709
Haojun Sui <shjnyr@me.com>
2015-11-16 23:27:14 -0500

    Implements client's desired acceleration feature, no deceleration yet.

--- Assets/SimView/SimViewCar.cs
+++ Assets/SimView/SimViewCar.cs
@@ -3,15 +3,13 @@ using System.Collections;
 
 public class SimViewCar : MonoBehaviour {
 	public SimViewRoad[] roads;
-	private int currRoadIndex = 0;
 
-	private float minSpeed = 1f;
-	private float maxSpeed = 5f;
-	public float speed = 1f;
-	private float alertDistance = 3f;
+	private float maxSpeed = 3f;
+	private float speed = 0.0f;
 	private float angle;
-	private float currentTravelDistance = 0.0f;
-	private float currRoadLength;
+	private float reactionTime = 0.2f;
+	private float acce = 0.02f;
+	private float deac = -0.5f;
 	
 	private Vector3 origin;
 	private Vector3 destination;
@@ -19,8 +17,10 @@ public class SimViewCar : MonoBehaviour {
 	private Vector3[] path = {new Vector3(4, -3, -2), new Vector3(2, 4, -2), new Vector3(-3, -4, -2), new Vector3(7, 1, -2), new Vector3(-3, 3, -2), new Vector3(4, -3, -2)};
 
 	private int i = 2;
+
 	// Use this for initialization
 	void Start () {
+		origin = path [0];
 		transform.position = path [0];
 		destination = path [1];
 		setAngle ();
@@ -30,20 +30,24 @@ public class SimViewCar : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (transform.position == destination && i <= 5) {
+			origin = path [i - 1];
 			destination = path [i];
 			setAngle ();
 			transform.eulerAngles = new Vector3 (0, 0, angle);
-			speed = 1f;
 			i++;
+			speed = 0;
 		}
+		speed = getSpeed ();
+
 		transform.position = Vector3.MoveTowards (transform.position, destination, speed * Time.deltaTime);
-		if (Vector3.Distance (transform.position, destination) > alertDistance && speed < maxSpeed)
-			speed += 1f * Time.deltaTime;
-		else if (Vector3.Distance (transform.position, destination) < alertDistance && speed > minSpeed)
-			speed -= 2f * Time.deltaTime;
+
 		// Test
 		if (i == 6)
 			i = 1;
+	}
+
+	public float getSpeed() {
+		return speed + 2.5f * acce * reactionTime * (1 - speed / maxSpeed) * Mathf.Sqrt (0.025f + speed / maxSpeed);
 	}
 
 	public void setAngle() {
